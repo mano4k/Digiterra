5fa86926c6f63a02da263b43ef005364356e31b9
mano <rod15ilboudo@gmail.com>
2025-07-06 23:30:44 -0500

    version 2

--- app.py
+++ app.py
@@ -5,13 +5,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-# @app.route('/services')
-# def service():
-#     return render_template('services.html')
+@app.route('/services')
+def service():
+    return render_template('services.html')
 
-# @app.route('/contact')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# @app.route('/propos')
 # def contact():
-#     return render_template('contact.html')
+#     return render_template('propos.html')
+
+@app.route('/vente')
+def vente():
+    return render_template('vente.html')
+
+
 
 
 if __name__ == '__main__':
